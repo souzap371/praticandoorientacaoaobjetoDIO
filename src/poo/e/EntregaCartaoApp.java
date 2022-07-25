f3591d90880f1d1013ff256e21db8d4e7d09c7db
Pablo Souza <souzap371@gmail.com>
2022-07-24 21:04:42 -0300

    Praticando Orientação a objeto, criação de classes e aplicação de metodos

---
+++ src/poo/e/EntregaCartaoApp.java
@@ -0,0 +1,26 @@
+package poo.e;
+
+import poo.model.Cliente;
+import poo.model.Endereco;
+
+import java.util.ArrayList;
+
+public class EntregaCartaoApp {
+
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.cep = "000000";
+        // dados do endereco
+
+        Cliente cliente = new Cliente();
+        // dados do cliente
+
+        try {
+            cliente.adicionaEndereco(endereco);
+            System.out.println("Endereco adicionado com sucesso!");
+        }catch (Exception e) {
+            System.err.println("Houve um erro ao adicionar o endereço: " + e.getMessage());
+        }
+    }
+
+}
